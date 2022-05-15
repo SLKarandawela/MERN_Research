5b34f08c638051d9fa20123929cf530f2d1e5b61
MiharaEk <mipinuri98@gmail.com>
2022-05-15 19:43:56 +0530

    models

---
+++ backend/auth/models/Staff.js
@@ -0,0 +1,71 @@
+const crypto = require("crypto");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const StaffSchema = new mongoose.Schema({
+    staffid:{
+        type: String,
+        required: [true, "Please Provide Staff ID"],
+        unique:true
+    },
+    stafffirstname:{
+        type: String,
+        required: [true, "Please Provide a First Name"]
+    },
+    stafflastname:{
+        type: String,
+        required: [true, "Please Provide a Last Name"]
+    },
+    staffemail:{
+        type: String,
+        required: [true, "Please Provide Staff Email"],
+        unique:true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid staff email",
+        ]
+    },
+    contactnumber:{
+        type: String,
+        required: [true, "Please Provide a Contact Number"]
+    },
+    faculty:{
+        type: String,
+        required: [true, "Please Provide a Faculty"]
+    },
+    researcharea:{
+        type: String,
+        required: [true, "Please Provide a Research Area"]
+    },
+    password:{
+        type: String,
+        required: [true, "Please Provide a Password"],
+        minlength:6,
+        select:false
+    },
+    resetPasswordtoken: String,
+    resetPasswordExpire: Date
+});
+
+StaffSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+});
+
+StaffSchema.methods.matchPasswords = async function(password){
+    return await bcrypt.compare(password, this.password);
+};
+
+StaffSchema.methods.getSignedToken = function(){
+    return jwt.sign({id:this._id}, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE,});
+};
+
+const Staff = mongoose.model("Staff", StaffSchema);
+
+module.exports = Staff;
---
+++ backend/auth/models/Student.js
@@ -0,0 +1,80 @@
+const crypto = require("crypto");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const StudentSchema = new mongoose.Schema({
+    studentid:{
+        type: String,
+        required: [true, "Please Provide Student ID"],
+        unique:true
+    },
+    studentfirstname:{
+        type: String,
+        required: [true, "Please Provide Student First Name"]
+    },
+    studentlastname:{
+        type: String,
+        required: [true, "Please Provide Student Last Name"]
+    },
+    studentemail:{
+        type: String,
+        required: [true, "Please Provide Student Email"],
+        unique:true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid student email",
+        ]
+    },
+    studentpersonalemail:{
+        type: String,
+        required: [true, "Please Provide Personal Email"],
+        unique:true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid personal email",
+        ]
+    },
+    contactnumber:{
+        type: String,
+        required: [true, "Please Provide a Contact Number"]
+    },
+    degree:{
+        type: String,
+        required: [true, "Please Provide a Degree"]
+    },
+    specialization:{
+        type: String,
+        required: [true, "Please Provide a Specialization"]
+    },
+    password:{
+        type: String,
+        required: [true, "Please Provide a Password"],
+        minlength:6,
+        select:false
+    },
+    resetPasswordtoken: String,
+    resetPasswordExpire: Date
+});
+
+StudentSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+});
+
+StudentSchema.methods.matchPasswords = async function(password){
+    return await bcrypt.compare(password, this.password);
+};
+
+StudentSchema.methods.getSignedToken = function(){
+    return jwt.sign({id:this._id}, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE,});
+};
+
+const Student = mongoose.model("Student", StudentSchema);
+
+module.exports = Student;
