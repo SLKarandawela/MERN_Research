47ddeb778efa3f6951c2f8e420c1b0b8d8e6291b
MiharaEk <mipinuri98@gmail.com>
2022-05-15 18:37:00 +0530

    path, body parser and error handler

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,28 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
+
+
+const bodyParser = require('body-parser');
+
+const errorHandler = require('./auth/middleware/error');
+
 dotenv.config();
-// connecting to the database
+//connecting to the database
 mongoose.connect
 (process.env.MONGO_URL).then(() => console.log("Mongodb database connected successfully")).catch((e) => {
     console.log(e)
 
 });
+
+app.use(bodyParser.json())
+
+app.use(express.json());
+
+app.use('/api/auth', require('./auth/routes/auth'));
+
+//Error Handler (should be the last piece of middleware)
+app.use(errorHandler);
 
 app.listen(3000, () =>{
     console.log("Backend sevice is listening on port 3000 ")
