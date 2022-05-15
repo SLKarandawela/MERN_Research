6e4928db5187dc835258997f9706857cf4465d2b
MiharaEk <mipinuri98@gmail.com>
2022-05-15 19:43:30 +0530

    err

--- backend/auth/middleware/error.js
+++ backend/auth/middleware/error.js
@@ -7,16 +7,16 @@ const errorHandler = (err, req, res, next) => {
 
     if(err.code === 11000){
         //duplicate error key
-        const message = `Duplicate Field Vallue Entered`;
+        const message = `Duplicate Field Value Entered`;
         error = new ErrorResponse(message,400);
     }
 
-    if(error.name === "ValidationError"){
+    if(err.name === "ValidationError"){
         const message = Object.values(err.errors).map((val) => val.message);
         error = new ErrorResponse(message, 400);
     }
 
-    res.status(err.statusCode || 500).json({
+    res.status(error.statusCode || 500).json({
         success:false,
         error: error.message || "Server Error",
     });
